e48da7d653653b6b8b48a8cc1d656bd04e0d710d
Prerna <>
2022-02-12 00:26:31 +0530

    Attached View with ViewModel and Adapter class

--- app/src/main/java/com/example/couroutinewithroom/MainActivity.kt
+++ app/src/main/java/com/example/couroutinewithroom/MainActivity.kt
@@ -3,20 +3,40 @@ package com.example.couroutinewithroom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.couroutinewithroom.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var personViewModel: PersonViewModel
-   // private lateinit var personAdapter: PersonAdapter
+    private lateinit var personAdapter: PersonAdapter
     private lateinit var personviewModelFactory: PersonviewModelFactory
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(this@MainActivity, RecyclerView
+                .VERTICAL)
+        )
         personviewModelFactory = PersonviewModelFactory(application)
-        personViewModel = ViewModelProvider(this,personviewModelFactory).get(PersonViewModel::class.java)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        personViewModel =
+            ViewModelProvider(this, personviewModelFactory).get(PersonViewModel::class.java)
+        personViewModel.getallPersonsList().observe(this, Observer<List<PersonListItem>> { personListItem ->
+            personAdapter = PersonAdapter(personListItem as MutableList<PersonListItem>)
+            binding.recyclerView.adapter = personAdapter
+          //  personAdapter.setData(personListItem)
+        })
+
+//        personAdapter = PersonAdapter(mutableListOf())
+
+
     }
 }
